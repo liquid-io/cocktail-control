90a75362eb8a546027f142ffb9f0c4543eb50337
Matteo Collina <hello@matteocollina.com>
2015-09-02 20:07:27 +0100

    Added state with levelup

--- index.js
+++ index.js
@@ -2,25 +2,73 @@
 
 var EE = require('events').EventEmitter
 var clone = require('clone')
+var fastparallel = require('fastparallel')
+var JOBS = 'JOBS::'
 
-function cocktailControl (defs) {
+function cocktailControl (db, defs) {
   var that = new EE()
+  var parallel = fastparallel()
 
-  that.enqueue = enqueue
-  that.free = free
+  that.enqueue = deferredEnqueue
+  that.free = deferredFree
 
   var jobs = []
   var count = 0
+  var deferred = []
+
+  var stream = db.createValueStream({
+    gt: JOBS,
+    lt: JOBS + '\xff',
+    valueEncoding: 'json'
+  })
+
+  stream.on('data', function (job) {
+    count = job.id
+    jobs.push(job)
+  })
+
+  stream.on('end', function () {
+    that.enqueue = enqueue
+    that.free = free
+
+    parallel(that, function (task, cb) {
+      this[task.method](task.job, function (err) {
+        if (task.cb) {
+          task.cb(err)
+        }
+        cb(err)
+      })
+    }, deferred, function (err) {
+      if (err) {
+        that.emit('error', err)
+      }
+
+      that.emit('ready')
+    })
+  })
 
   return that
 
-  function enqueue (job) {
-    job.id = count++
-    jobs.push(job)
+  function deferredEnqueue (job, cb) {
+    deferred.push({ method: 'enqueue', job: job, cb: cb })
     return this
   }
 
-  function free (worker) {
+  function deferredFree (worker, cb) {
+    deferred.push({ method: 'free', job: worker, cb: cb })
+    return this
+  }
+
+  function enqueue (job, cb) {
+    job.id = count++
+    jobs.push(job)
+    db.put(JOBS + job.id, job, {
+      valueEncoding: 'json'
+    }, cb)
+    return this
+  }
+
+  function free (worker, cb) {
     var wrap = defs.workers[worker]
 
     if (!wrap) {
@@ -55,13 +103,22 @@ function cocktailControl (defs) {
       }
     })
 
+    db.batch(executable.map(function (job) {
+      return {
+        type: 'del',
+        key: JOBS + job.id
+      }
+    }), cb)
+
     jobs = jobs.filter(function (job) {
       return executable.indexOf(job) < 0
     })
 
-    that.emit(worker, {
-      jobs: executable
-    })
+    if (executable.length > 0) {
+      that.emit(worker, {
+        jobs: executable
+      })
+    }
 
     return this
   }
--- test.js
+++ test.js
@@ -2,37 +2,44 @@
 
 var test = require('tape')
 var control = require('./')
+var level = require('level')
+var tmp = require('tmp')
+var parallel = require('fastparallel')()
+
+var cocktails = {
+  bloody: {
+    activations: [
+      20, // milleseconds
+      10,
+      50
+    ]
+  },
+  spritz: {
+    activations: [
+      10, // milleseconds
+      20,
+      30
+    ]
+  }
+}
+var defs = {
+  cocktails: cocktails,
+  workers: {
+    bob: {
+      cocktails: ['bloody', 'spritz']
+    },
+    mark: {
+      cocktails: ['spritz', 'spritz']
+    }
+  }
+}
 
 test('work some cocktails', function (t) {
   t.plan(3)
 
-  var defs = {
-    cocktails: {
-      bloody: {
-        activations: [
-          20, // milleseconds
-          10,
-          50
-        ]
-      },
-      spritz: {
-        activations: [
-          10, // milleseconds
-          20,
-          30
-        ]
-      }
-    },
-    workers: {
-      bob: {
-        cocktails: ['bloody', 'spritz']
-      },
-      mark: {
-        cocktails: ['spritz', 'spritz']
-      }
-    }
-  }
-  var robot = control(defs)
+  var dir = tmp.dirSync()
+  var db = level(dir.name)
+  var robot = control(db, defs)
 
   robot.enqueue({
     cocktail: 'spritz',
@@ -111,4 +118,98 @@ test('work some cocktails', function (t) {
   // kick off processing
   robot.free('bob') // let says that bob is free
   robot.free('mark') // let says that mark is free
+})
+
+test('restore state', function (t) {
+  t.plan(3)
+
+  var dir = tmp.dirSync()
+  var db = level(dir.name)
+  var robot = control(db, defs)
+
+  parallel(null, function (job, cb) {
+    robot.enqueue(job, cb)
+  }, [{
+    cocktail: 'spritz',
+    name: 'Matteo'
+  }, {
+    cocktail: 'spritz',
+    name: 'Cian'
+  }, {
+    cocktail: 'spritz',
+    name: 'Tammy'
+  }, {
+    cocktail: 'bloody',
+    name: 'David'
+  }, {
+    cocktail: 'bloody',
+    name: 'Richard'
+  }],  function done () {
+
+    var robot = control(db, defs)
+
+    robot.once('bob', function (cocktails) {
+      t.deepEqual(cocktails, {
+        jobs: [{
+          id: 0,
+          name: 'Matteo',
+          cocktail: 'spritz',
+          pump: 1,
+          activations: defs.cocktails.spritz.activations
+        }, {
+          id: 3,
+          name: 'David',
+          cocktail: 'bloody',
+          pump: 0,
+          activations: defs.cocktails.bloody.activations
+        }]
+      }, 'cocktails enqueued to bob matches')
+
+      // this would be async in the real world
+      setImmediate(robot.free.bind(robot, 'bob'))
+
+      robot.once('bob', function (cocktails) {
+        t.deepEqual(cocktails, {
+          jobs: [{
+            id: 4,
+            name: 'Richard',
+            cocktail: 'bloody',
+            pump: 0,
+            activations: defs.cocktails.bloody.activations
+          }]
+        }, 'cocktails enqueued to bob matches')
+
+        // this would be async in the real world
+        setImmediate(robot.free.bind(robot, 'bob'))
+      })
+    })
+
+    robot.once('mark', function (cocktails) {
+      t.deepEqual(cocktails, {
+        jobs: [{
+          id: 1,
+          name: 'Cian',
+          cocktail: 'spritz',
+          pump: 0,
+          activations: defs.cocktails.spritz.activations
+        }, {
+          id: 2,
+          name: 'Tammy',
+          cocktail: 'spritz',
+          pump: 1,
+          activations: defs.cocktails.spritz.activations
+        }]
+      }, 'cocktails enqueued to mark matches')
+
+      // this would be async in the real world
+      setImmediate(robot.free.bind(robot, 'mark'))
+    })
+
+    robot.on('ready', function () {
+      // kick off processing
+      robot.free('bob') // let says that bob is free
+      robot.free('mark') // let says that mark is free
+    })
+  })
+
 })
